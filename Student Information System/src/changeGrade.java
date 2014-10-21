c3d15435e0ad2fd20b3e6985204230d5e995d44a
gursev.bajwa <gursev.bajwa@MU-C3-02.mullen.pvt.k12.co.us>
2014-10-21 08:41:31 -0600

    ll

--- Student Information System/src/changeGrade.java
+++ Student Information System/src/changeGrade.java
@@ -8,9 +8,8 @@ public class changeGrade
 	static String selectClass;
 
 	//Call the file
-	//Ask user to select student
-	//Then ask for what class
-	//ll
+	
+	//l
 	//Replace the original grade with the user input
 public static void GradeChange()
 	{
@@ -20,6 +19,7 @@ public static void GradeChange()
 	selectStudent = userInput.nextLine();
 	//Display student's classes
 	//Select class
+	System.out.println("For what class?");
 	Scanner userInput1 = new Scanner(System.in);
 	selectClass = userInput.nextLine();
 	//Display changed Class grades 
