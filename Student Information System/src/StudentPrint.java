5ec2f25b1adadd6562d9d33ecab5caca5c743d30
justin.herrera <justin.herrera@MU-C3-04.mullen.pvt.k12.co.us>
2014-10-22 10:13:05 -0600

    Trying Again

---
+++ Student Information System/src/StudentPrint.java
@@ -0,0 +1,98 @@
+public class StudentPrint
+	{
+	String firstNamew;
+	String lastNamew;
+	String GPAstrw;
+	String p1Classw;
+	String p1Gradew;
+	String p2Classw;
+	String p2Gradew;
+	String p3Classw;
+	String p3Gradew;
+
+	public StudentPrint(String fnz, String lnz, String stgz, String p1cz, String p1gz, String p2cz, String p2gz, String p3cz, String p3gz)
+		{
+		firstNamew = fnz;
+		lastNamew = lnz;
+		GPAstrw = stgz;
+		p1Classw = p1cz;
+		p1Gradew = p1gz;
+		p2Classw = p2cz;
+		p2Gradew = p2gz;
+		p3Classw = p3cz;
+		p3Gradew = p3gz;
+			}
+
+		public String getFirstNamew() {
+			return firstNamew;
+		}
+	
+		public String getLastNamew() {
+			return lastNamew;
+		}
+	
+		public String getGPAstrw() {
+			return GPAstrw;
+		}
+	
+		public String getP1Classw() {
+			return p1Classw;
+		}
+	
+		public String getP1Gradew() {
+			return p1Gradew;
+		}
+	
+		public String getP2Classw() {
+			return p2Classw;
+		}
+	
+		public String getP2Gradew() {
+			return p2Gradew;
+		}
+	
+		public String getP3Classw() {
+			return p3Classw;
+		}
+	
+		public String getP3Gradew() {
+			return p3Gradew;
+		}
+	
+		public void setFirstNamew(String firstNamew) {
+			this.firstNamew = firstNamew;
+		}
+	
+		public void setLastNamew(String lastNamew) {
+			this.lastNamew = lastNamew;
+		}
+	
+		public void setGPAstrw(String gPAstrw) {
+			GPAstrw = gPAstrw;
+		}
+	
+		public void setP1Classw(String p1Classw) {
+			this.p1Classw = p1Classw;
+		}
+	
+		public void setP1Gradew(String p1Gradew) {
+			this.p1Gradew = p1Gradew;
+		}
+	
+		public void setP2Classw(String p2Classw) {
+			this.p2Classw = p2Classw;
+		}
+	
+		public void setP2Gradew(String p2Gradew) {
+			this.p2Gradew = p2Gradew;
+		}
+	
+		public void setP3Classw(String p3Classw) {
+			this.p3Classw = p3Classw;
+		}
+	
+		public void setP3Gradew(String p3Gradew) {
+			this.p3Gradew = p3Gradew;
+		}
+	
+	}
